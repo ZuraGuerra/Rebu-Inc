0d8aeb9168ad206fa1a9332e1df0d6c3fcaf7dab
Juan Fragoso <juan_fragoso@me.com>
2015-03-12 19:04:56 -0600

    parse conectado fbloggin te lleva a las pantallas correctas

--- Rebu/ViewController.swift
+++ Rebu/ViewController.swift
@@ -17,12 +17,21 @@ class ViewController: UIViewController {
             (user: PFUser!, error: NSError!) -> Void in
             if user == nil {
                 NSLog("Uh oh. The user cancelled the Facebook login.")
+                var Register : UINavigationController = self.storyboard!.instantiateViewControllerWithIdentifier("Register") as UINavigationController
+                
+                self.navigationController!.pushViewController(Register, animated: true)
+                
+                
             } else if user.isNew {
                 NSLog("User signed up and logged in through Facebook!")
                 
             } else {
                 NSLog("User logged in through Facebook!")
-                self.performSegueWithIdentifier("MapScreen", sender: self)
+               // self.performSegueWithIdentifier("MapScreen", sender: self)
+                
+                var MapScreen : UIViewController = self.storyboard!.instantiateViewControllerWithIdentifier("MapScreen") as UIViewController
+                
+                self.navigationController!.pushViewController(MapScreen, animated: true)
             }
         })
     }
@@ -31,6 +40,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // save objects to parse
+        var object = PFObject(className: "testDataClass")
+        object.addObject("iOSBlog", forKey: "websiteUrl")
+        object.addObject("Five", forKey: "websiteRating")
+        object.save()
+        
+        var object2 = PFObject(className: "testDataClass")
+        object2.addObject("MEAN book", forKey: "websiteUrl")
+        object2.addObject("Four", forKey: "websiteRating")
+        object2.save()
     }
 
     override func didReceiveMemoryWarning() {
